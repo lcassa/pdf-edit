42109ddf7502b9a0d26fad4131696af11beace77
Leandro Cassa <leandro.cassa@digibee.com.br>
2021-03-07 13:46:20 -0300

    try google auth

--- api/index.js
+++ api/index.js
@@ -59,12 +59,18 @@ const SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
 // time.
 const TOKEN_PATH = 'token.json'
 
-// Load client secrets from a local file.
-// fs.readFile('credentials.json', (err, content) => {
-//   if (err) return console.log('Error loading client secret file:', err)
-//   // Authorize a client with credentials, then call the Google Drive API.
-//   authorize(JSON.parse(content), listFiles)
-// })
+const auth = {
+    "web": {
+        "client_id": process.env.CLIENT_ID,
+        "project_id": "pdf-edit-1615113808913",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_secret": process.env.CLIENT_SECRET,
+        "redirect_uris":["https://pdf-edit.vercel.app/api"]
+    }
+}
+
 
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
@@ -140,6 +146,10 @@ function listFiles(auth) {
 function retrieveFileBytes(file) {
     //
 }
+
+
+// authorize
+authorize(auth, listFiles)
 
 module.exports = (req, res) => {
     console.log(">>> THIS IS ON THE LOGS")
