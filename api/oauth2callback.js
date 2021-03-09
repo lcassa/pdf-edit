e22525b1e3892573ac9dce88081973edc0592016
Leandro Cassa <leandro.cassa@digibee.com.br>
2021-03-08 22:28:00 -0300

    testing

--- api/oauth2callback.js
+++ api/oauth2callback.js
@@ -19,7 +19,7 @@ const credentials = {
         "token_uri":"https://oauth2.googleapis.com/token",
         "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
         "client_secret":process.env.CLIENT_SECRET,
-        "redirect_uris":["https://pdf-edit.vercel.app/api/auth"],
+        "redirect_uris":["https://pdf-edit.vercel.app/api/oath2callback"],
         "javascript_origins":["https://pdf-edit.vercel.app"]
     }
 }
